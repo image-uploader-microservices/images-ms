78c217064020c45ba7a601ca27ae3fc6f3b1c267
Yeattt <nekz.8896@gmail.com>
2024-04-25 22:26:26 -0500

    Nats protocol added

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger: Logger = new Logger('Images-Microservice')
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers,
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,7 +22,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(envs.port);
+  await app.listen();
 
   logger.log(`Images Microservice running on port ${envs.port}`);
 }
